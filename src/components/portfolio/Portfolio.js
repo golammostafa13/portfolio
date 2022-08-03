05fcc3f92e5d576ea9529a6a71576fdaa0e77a4e
Golam Mostafa <mostafarmstu@gmail.com>
2022-08-04 02:19:10 +0600

    update

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,41 +1,60 @@
 import React from 'react'
 import { Col, Container, Row } from 'reactstrap'
-import img01 from './image1-weather.png';
-import img02 from './image2-odin-ecom.png';
-import img03 from './image4-cv.png';
-import img04 from './img7.png'
-import img05 from './img8.png';
+import weather from './image1-weather.png';
+import ecom from './e-com.jpeg';
+import cv from './cv.jpeg';
+import lyrics from './img7.png'
+import academy from './img8.png';
 import img06 from './img6.png';
-
+import NMC from './nasa.jpeg';
+import quiz from './quiz.jpeg';
 import './Portfolio.css'
 
 const portfolioData = [
   {
-    imgUrl: img01,
-    title: 'World cities weather JS app',
-    url: 'https://weather-app-mostafa.netlify.app'
+    type: 'Backend Development',
+    imgUrl: NMC,
+    title: 'NASA Mission Control Node js App',
+    url: 'https://rocky-fjord-66825.herokuapp.com/',
   },
   {
-    imgUrl: img02,
+    type: 'Frontend Development',
+    imgUrl: quiz,
+    title: 'Quiz React App',
+    url: 'https://golammostafa13.github.io/quize-app/',
+  },
+  {
+    type: 'Frontend Development',
+    imgUrl: cv,
+    title: 'Create Your own CV',
+    url: 'https://golammostafa13.github.io/cv-creator/',
+  },
+  {
+    type: 'Frontend Development',
+    imgUrl: ecom,
     title: 'Simple e-commerce React app',
     url: 'https://e-commerce-mostafa.netlify.app'
   },
   {
-    imgUrl: img03,
-    title: 'CV-creator React app',
-    url: 'https://cv-create.netlify.app/'
+    type: 'Frontend Development',
+    imgUrl: weather,
+    title: 'World cities weather JS app',
+    url: 'https://weather-app-mostafa.netlify.app'
   },
   {
-    imgUrl: img04,
+    type: 'Frontend Development',
+    imgUrl: lyrics,
     title: 'Lyrcis search with word JS app',
     url: 'https://hard-rock-api.netlify.app'
   },
   {
-    imgUrl: img05,
+    type: 'Frontend Development',
+    imgUrl: academy,
     title: 'Academic website HTML and CSS',
     url: 'https://nurkindergarten.netlify.app'
   },
   {
+    type: 'Frontend Development',
     imgUrl: img06,
     title: 'Todo JS app',
     url: 'https://todo-list-mostafa.netlify.app'
@@ -53,7 +72,7 @@ export const Portfolio = () => {
 
           {
             portfolioData.map((item, idx) =>(
-              <Col lg='4' md='6' sm='6' key={idx}>
+              <Col lg='3' md='6' sm='6' key={idx}>
                 <div className='portfolio_card'>
                   <div className='portfolio_img'>
                   <img src={item.imgUrl} alt="" className='w-100' />
@@ -61,10 +80,10 @@ export const Portfolio = () => {
 
                   <div className='portfolio_content'>
                     <div className='content_top'>
-                      <h6>Frontend Development</h6>
+                      <h6>{item.type}</h6>
                     </div>
                     <h5>{item.title}</h5>
-                    <a href={item.url}>View live demo</a>
+                    <a href={item.url} target="_blank">View live demo</a>
                   </div>
                 </div>
               </Col>
